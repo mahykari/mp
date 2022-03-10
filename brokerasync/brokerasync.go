67ec9e495ac164152edd126a4ac10bff7af25c2a
moyarka <mahyar.karimi2079@gmail.com>
2022-03-10 20:07:19 +0330

    add brokerasync.go

---
+++ brokerasync/brokerasync.go
@@ -0,0 +1,36 @@
+package brokerasync
+
+const BUFFER_SZ = 8
+
+type BrokerAsync struct {
+	buffer chan string
+}
+
+func NewBrokerAsync() *BrokerAsync {
+	return &BrokerAsync{
+		buffer: make(chan string, BUFFER_SZ),
+	}
+}
+
+func (b *BrokerAsync) Send(msg string) <-chan bool {
+	done := make(chan bool)
+
+	go func() {
+		defer close(done)
+		b.buffer <- msg
+		done <- true
+	}()
+
+	return done
+}
+
+func (b *BrokerAsync) Receive() <-chan string {
+	ch := make(chan string)
+
+	go func() {
+		defer close(ch)
+		ch <- <-b.buffer
+	}()
+
+	return ch
+}
