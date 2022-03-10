c0712256b61ded6ff16248d3c7a317fd742b087a
moyarka <mahyar.karimi2079@gmail.com>
2022-03-10 23:18:03 +0330

    add bidir_test.go for brokersync

---
+++ brokersync/bidir_test.go
@@ -0,0 +1,33 @@
+package brokersync
+
+import (
+	"log"
+	"sync"
+	"testing"
+	"time"
+)
+
+func TestBidir(t *testing.T) {
+	bs := NewBrokerSync()
+
+	wg := &sync.WaitGroup{}
+	wg.Add(2)
+
+	go func() {
+		defer wg.Done()
+		bs.Send("test0")
+		log.Printf("0: Sent 'test0'")
+		time.Sleep(time.Millisecond * 200)
+		log.Printf("0: Received '%s'", bs.Receive())
+	}()
+
+	go func() {
+		defer wg.Done()
+		time.Sleep(time.Millisecond * 100)
+		log.Printf("1: Received '%s'", bs.Receive())
+		bs.Send("test1")
+		log.Printf("1: Sent 'test1'")
+	}()
+
+	wg.Wait()
+}
