9302dd604b2c79e1786d9b2a75492d9b5d219090
moyarka <mahyar.karimi2079@gmail.com>
2022-03-10 19:11:29 +0330

    add brokersync.go

---
+++ brokersync/brokersync.go
@@ -0,0 +1,21 @@
+package brokersync
+
+const BUFFER_SZ = 8
+
+type BrokerSync struct {
+	buffer chan string
+}
+
+func NewBrokerSync() *BrokerSync {
+	return &BrokerSync{
+		buffer: make(chan string, BUFFER_SZ),
+	}
+}
+
+func (b *BrokerSync) Send(msg string) {
+	b.buffer <- msg
+}
+
+func (b *BrokerSync) Receive() string {
+	return <-b.buffer
+}
